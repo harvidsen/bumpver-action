139a871e17967a50b56f3380a4e313d34a4b718f
harvidsen <hakon.arvidsen@eviny.no>
2022-08-24 09:55:19 +0200

    Let checkout action set safe.directory git config

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@ set -e
 cd "${GITHUB_WORKSPACE}"
 
 echo "Configuring git"
-git config --global --add safe.directory "${GITHUB_WORKSPACE}"
 git config --global user.email $1
 git config --global user.name $2
 
